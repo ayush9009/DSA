ea49feefbbbe248eb09690fd6bbe39da8cd541c6
ayush9009 <97290036+ayush9009@users.noreply.github.com>
2023-03-05 11:16:10 +0530

    Create LC PassThePillow.cpp

---
+++ LC PassThePillow.cpp
@@ -0,0 +1,92 @@
+class Solution {
+public:
+    void merge(int a[], int beg, int mid, int end)    
+{    
+    int i, j, k;  
+    int n1 = mid - beg + 1;    
+    int n2 = end - mid;    
+      
+    int LeftArray[n1], RightArray[n2]; //temporary arrays  
+      
+    /* copy data to temp arrays */  
+    for (int i = 0; i < n1; i++)    
+    LeftArray[i] = a[beg + i];    
+    for (int j = 0; j < n2; j++)    
+    RightArray[j] = a[mid + 1 + j];    
+      
+    i = 0; /* initial index of first sub-array */  
+    j = 0; /* initial index of second sub-array */   
+    k = beg;  /* initial index of merged sub-array */  
+      
+    while (i < n1 && j < n2)    
+    {    
+        if(LeftArray[i] <= RightArray[j])    
+        {    
+            a[k] = LeftArray[i];    
+            i++;    
+        }    
+        else    
+        {    
+            a[k] = RightArray[j];    
+            j++;    
+        }    
+        k++;    
+    }    
+    while (i<n1)    
+    {    
+        a[k] = LeftArray[i];    
+        i++;    
+        k++;    
+    }    
+      
+    while (j<n2)    
+    {    
+        a[k] = RightArray[j];    
+        j++;    
+        k++;    
+    }    
+}    
+    int passThePillow(int n, int time) {
+//         int i=1;
+//         int flag=0;
+//         while(time >0 ){
+//             if(i==n && time >0){
+//                 flag=-1;
+//                 time--;
+//                 i--;
+//             }
+//             else {
+//                 i++;
+//                 time--;
+//             }
+//         }
+     
+        
+//         cout<<i<<endl;
+//         return i;
+        int leftium=1;
+        int cntium=1;
+        while(time--){
+            if(leftium==1){
+                cntium=1;
+            }
+            if(leftium==n){
+                cntium=-1;
+            }
+           
+            leftium+=cntium;
+        }
+        return leftium;
+    }
+    void mergeSort(int a[], int beg, int end)  
+{  
+    if (beg < end)   
+    {  
+        int mid = (beg + end) / 2;  
+        mergeSort(a, beg, mid);  
+        mergeSort(a, mid + 1, end);  
+        merge(a, beg, mid, end);  
+    }  
+}  
+  
+};
