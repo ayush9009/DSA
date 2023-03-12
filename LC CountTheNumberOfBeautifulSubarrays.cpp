c0f627d5133153d7cb7050aa4e19b4bb33442522
ayush9009 <97290036+ayush9009@users.noreply.github.com>
2023-03-12 13:04:36 +0530

    Create LC CountTheNumberOfBeautifulSubarrays.cpp

---
+++ LC CountTheNumberOfBeautifulSubarrays.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    void merge(int a[], int beg, int mid, int end)    
+{    
+    int i, j, k;  
+    int n1 = mid - beg + 1;    
+    int n2 = end - mid;    
+      
+    int LeftArray[n1], RightArray[n2]; //temporary arrays  
+      
+    /* copy data to temp arrays */  
+    for (int i = 0; i < n1; i++)    
+    LeftArray[i] = a[beg + i];    
+    for (int j = 0; j < n2; j++)    
+    RightArray[j] = a[mid + 1 + j];    
+      
+    i = 0; /* initial index of first sub-array */  
+    j = 0; /* initial index of second sub-array */   
+    k = beg;  /* initial index of merged sub-array */  
+      
+    while (i < n1 && j < n2)    
+    {    
+        if(LeftArray[i] <= RightArray[j])    
+        {    
+            a[k] = LeftArray[i];    
+            i++;    
+        }    
+        else    
+        {    
+            a[k] = RightArray[j];    
+            j++;    
+        }    
+        k++;    
+    }    
+    while (i<n1)    
+    {    
+        a[k] = LeftArray[i];    
+        i++;    
+        k++;    
+    }    
+      
+    while (j<n2)    
+    {    
+        a[k] = RightArray[j];    
+        j++;    
+        k++;    
+    }    
+}    
+    unordered_map<int, int> mp;
+    long long helper(vector<int>& tora,long long &ct_scan, long long &xr  ){
+        for(int i = 0; i < tora.size(); i++) {
+          xr ^= tora[i];
+          ct_scan += mp[xr];
+          mp[xr]++;
+       }
+        return ct_scan;
+    }
+    void mergeSort(int a[], int beg, int end)  
+{  
+    if (beg < end)   
+    {  
+        int mid = (beg + end) / 2;  
+        mergeSort(a, beg, mid);  
+        mergeSort(a, mid + 1, end);  
+        merge(a, beg, mid, end);  
+    }  
+}  
+    long long beautifulSubarrays(vector<int>& tora) {
+        long long ct_scan = 0, xr = 0;
+        mp[xr]++;
+        return helper(tora,ct_scan,xr);
+    
+    }
+};
