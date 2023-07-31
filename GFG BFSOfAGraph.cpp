3b2d81fe17ab97ef47407ae0e150eb5d2487d328
Ayush Sharma <97290036+ayush9009@users.noreply.github.com>
2023-07-31 23:30:22 +0530

    Create GFG BFSOfAGraph.cpp

---
+++ GFG BFSOfAGraph.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+   
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        vector<int>ans,vis(V,0);
+       
+        queue<int>q;
+        q.push(0);
+        vis[0]=1;
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            ans.push_back(it);
+            for(auto node : adj[it]){
+                if(!vis[node]){
+                    vis[node]=1;
+                    q.push(node);
+                }
+            }
+        }
+        return ans;
+    }
+};
