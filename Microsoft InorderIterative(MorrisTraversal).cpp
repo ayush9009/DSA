e3260fb08b3452f8194aac4886c2aaa2e6c875af
Ayush Sharma <97290036+ayush9009@users.noreply.github.com>
2023-08-22 15:02:41 +0530

    Create Microsoft InorderIterative(MorrisTraversal).cpp

---
+++ Microsoft InorderIterative(MorrisTraversal).cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+//MORRIS TRAVERSAL (Iterative Inorder)
+    vector<int> inOrder(Node* root)
+    {
+        //code here
+        vector<int>inorder;
+        Node* cur=root;
+        while(cur!=NULL){
+            if(cur->left==NULL){
+                inorder.push_back(cur->data);
+                cur=cur->right;
+            }else{
+                Node *prev=cur->left;
+                while(prev->right!=NULL && prev->right!=cur ){
+                    prev=prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right=cur;
+                    cur=cur->left;
+                }else{
+                    prev->right=NULL;
+                    inorder.push_back(cur->data);
+                    cur=cur->right;
+                }
+            }
+        }
+        return inorder;
+    }
+};
