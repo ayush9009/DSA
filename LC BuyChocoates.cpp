c3837687f818fb93f48c4afffd719ccdf9ccae3f
Ayush Sharma <97290036+ayush9009@users.noreply.github.com>
2023-12-20 23:11:59 +0530

    Create LC BuyChocoates.cpp

---
+++ LC BuyChocoates.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void rinko(){
+        return;
+    }
+    void mia_pia(){
+        return;
+    }
+    int buyChoco(vector<int>& p, int money) {
+        rinko();
+       sort(p.begin(), p.end()); 
+        mia_pia();
+        rinko();
+         
+        rinko();
+        int chingam = p[0] + p[1]; 
+        rinko();
+        mia_pia();
+         rinko();
+        if(money - chingam < 0) {
+            rinko();
+            mia_pia();
+             return money; 
+        }
+        rinko();
+        mia_pia();
+        return money - chingam;
+    }
+};
