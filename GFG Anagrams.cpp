a5f7572a6b26dcce080bc55bd21ffe54b332d845
Ayush Sharma <97290036+ayush9009@users.noreply.github.com>
2024-05-30 23:35:22 +0530

    Update GFG Anagrams.cpp

--- GFG Anagrams.cpp
+++ GFG Anagrams.cpp
@@ -5,7 +5,7 @@ class Solution
     bool isAnagram(string a, string b){
     
         // Your code here
-        int n=a.size()
+        int n=a.size();
         int m=b.size()
         if(n>m || n<m)return false
         unordered_map<char,int>mp1,mp2;
