ae35ecd8fbd6e43a90d204be1ec4483273200fee
Ayush Sharma <97290036+ayush9009@users.noreply.github.com>
2025-02-10 09:03:53 +0530

    Update GFG BFSOfAGraph.cpp

--- GFG BFSOfAGraph.cpp
+++ GFG BFSOfAGraph.cpp
@@ -3,7 +3,7 @@ class Solution {
     // Function to return Breadth First Traversal of given graph.
    
     vector<int> bfsOfGraph(int V, vector<int> adj[]) {
-        vector<int>ans,vis(V,0);
+        vector<int>ans,vis(V,0)
        
         queue<int>q;
         q.push(0
