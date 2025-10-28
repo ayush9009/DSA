9685b81f657fd0871102e0bf54ccd3e82562c523
Ayush Sharma <97290036+ayush9009@users.noreply.github.com>
2025-10-28 22:53:49 +0530

    Fix increment of left index in binary search

--- GFG BinarySearch.cpp
+++ GFG BinarySearch.cpp
@@ -13,7 +13,7 @@ class Solution {
                 right=right-1;
             }
             else{
-                left=left+1;
+                left=left+1
             }
         }
         return -1;
