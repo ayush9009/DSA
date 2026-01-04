032662606b458df98d284ac25e36ac0611f1ce0b
Ayush Sharma <97290036+ayush9009@users.noreply.github.com>
2026-01-04 23:55:17 +0530

    Update GFG AVLTreeDeletion.cpp

--- GFG AVLTreeDeletion.cpp
+++ GFG AVLTreeDeletion.cpp
@@ -4,7 +4,7 @@ function fetchData(){
          const data=// fetch data from ap
          if(data)
            resovle(data); 
-         }else
+         }els
            reject('eror data not found)
       })
       }
