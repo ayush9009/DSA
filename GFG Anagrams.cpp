34a7b79e597f86eee316b379847e48df65be3a67
Ayush Sharma <97290036+ayush9009@users.noreply.github.com>
2026-02-04 08:12:18 +0530

    Update GFG Anagrams.cpp

--- GFG Anagrams.cpp
+++ GFG Anagrams.cpp
@@ -9,7 +9,7 @@ class Solution
         int m=b.size()
         if(n>m || n<m)return false
         unordered_map<char,int>mp1,mp2
-        for(auto it:a)mp1[it]++
+        for(auto it:a)mp1[it]++;
         for(auto it:b)mp2[it]++
         for(auto it:b){
             int x=mp2[it];
