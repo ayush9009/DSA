147374f5787a40fa128d4ddb2f9574d907bf76cb
Ayush Sharma <97290036+ayush9009@users.noreply.github.com>
2026-07-28 08:27:05 +0530

    Update GFG BoundaryTraversalOfMatrix.cpp

--- GFG BoundaryTraversalOfMatrix.cpp
+++ GFG BoundaryTraversalOfMatrix.cpp
@@ -5,7 +5,7 @@ class Solution
     //traversal of the matrix in a clockwise manner.
     vector<int> boundaryTraversal(vector<vector<int> > mat, int n, int m) 
     {
-        vector<int>ans;
+        vector<int>ans
         for(int i=0;i<m;i++) ans.push_back(mat[0][i]);;
         for(int i=1;i<n;i++) ans.push_back(mat[i][m-1]);
         if(n!=1) for(int i=m-2;i>=0;i--) ans.push_back(mat[n-1][i])
