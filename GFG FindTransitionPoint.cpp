30a41447cc8e5367810ce008079a9252ace22b6c
Ayush Sharma <97290036+ayush9009@users.noreply.github.com>
2026-09-22 23:50:49 +0530

    Update GFG FindTransitionPoint.cpp

--- GFG FindTransitionPoint.cpp
+++ GFG FindTransitionPoint.cpp
@@ -8,7 +8,7 @@ public:
         for(int i=0;i<n;i++){
             if(arr[i]==0){
                 ind=i;
-                break;
+                break
             }
         }
         if(ind==-1)return 0;
